200a24552b5bfc906f26d4a6a1352649281b1ed2
Gaute Meek Olsen <gaute.meek@gmail.com>
2020-04-28 21:01:37 +0200

    end gif and pink color

--- vue/src/components/EndPlayer.vue
+++ vue/src/components/EndPlayer.vue
@@ -1,5 +1,10 @@
 <template>
   <h2>Thank you for playing!</h2>
+  <div class="gif">
+    <Gif v-if="rank === 0" type="won"/>
+    <Gif v-else-if="rank === 1 || rank === 2" type="close"/>
+    <Gif v-else type="lost"/>
+  </div>
   <Summary />
   <Fireworks v-if="rank === 0" size="10px" />
 </template>
@@ -10,18 +15,31 @@ import { computed } from 'vue';
 import { scores } from '@/utils/game'
 import { me } from '@/utils/player'
 import Fireworks from '@/components/Fireworks'
+import Gif from '@/components/Gif'
 
 export default {
   setup(){
     const rank = computed(() =>  scores.value.findIndex(p => p.uid === me.uid))
     return { rank };
   },
-  components: { Summary, Fireworks }
+  components: { Summary, Fireworks, Gif }
 }
 </script>
 
 <style scoped>
 h2{
   text-align: center;
+}
+
+.gif{
+  display: flex;
+  justify-content: center;
+  opacity: 0;
+  animation: show 2s ease-in-out 1s forwards;
+}
+
+@keyframes show {
+  0% { opacity: 0; }
+  100% { opacity: 1; }
 }
 </style>
--- vue/src/components/Gif.vue
+++ vue/src/components/Gif.vue
@@ -12,7 +12,8 @@ const imgs = {
     'https://media.giphy.com/media/5GoVLqeAOo6PK/giphy.gif',
     'https://media.giphy.com/media/xTiN0CNHgoRf1Ha7CM/giphy.gif',
     'https://media.giphy.com/media/cBQl0NCv0eZdS/giphy.gif',
-    'https://media.giphy.com/media/xeHH9NdRhAx5S/giphy.gif'
+    'https://media.giphy.com/media/xeHH9NdRhAx5S/giphy.gif',
+    'https://media.giphy.com/media/ZcKASxMYMKA9SQnhIl/giphy.gif',
   ],
   wrong: [
     'https://media.giphy.com/media/Ty9Sg8oHghPWg/giphy.gif',
@@ -24,7 +25,8 @@ const imgs = {
     'https://media.giphy.com/media/3o7btNRTJ700Vzmn5e/giphy.gif',
     'https://media.giphy.com/media/10UHehEC098kAE/giphy.gif',
     'https://media.giphy.com/media/l4pLY0zySvluEvr0c/giphy.gif',
-    'https://media.giphy.com/media/JnAydKs2GZRqTb4C8v/giphy.gif'
+    'https://media.giphy.com/media/JnAydKs2GZRqTb4C8v/giphy.gif',
+    'https://media.giphy.com/media/12rTjvF0ANQJsk/giphy.gif'
   ],
   waiting: [
     'https://media.giphy.com/media/W3a0zO282fuBpsqqyD/giphy.gif',
@@ -37,6 +39,38 @@ const imgs = {
     'https://media.giphy.com/media/BMtGb8JSk2Ln1cnPMA/giphy.gif',
     'https://media.giphy.com/media/ZFhhpKngh5QfcmhIDF/giphy.gif'
   ],
+  won: [
+    'https://media.giphy.com/media/xT0GqssRweIhlz209i/giphy.gif',
+    'https://media.giphy.com/media/3ohryhNgUwwZyxgktq/giphy.gif',
+    'https://media.giphy.com/media/dUYddwtCVUOIpgXijq/giphy.gif',
+    'https://media.giphy.com/media/3kD720zFVu22rfIA0s/giphy.gif',
+    'https://media.giphy.com/media/gd0Dqg6rYhttBVCZqd/giphy.gif',
+    'https://media.giphy.com/media/9wcu6Tr1ecmxa/giphy.gif',
+    'https://media.giphy.com/media/amrNGnZUeWhZC/giphy.gif',
+    'https://media.giphy.com/media/5fQyd7jM58m5y/giphy.gif',
+  ],
+  close: [
+    'https://media.giphy.com/media/3PAGCj08nkQvsooWUB/giphy.gif',
+    'https://media.giphy.com/media/Qaxd5XpgDWMso/giphy.gif',
+    'https://media.giphy.com/media/dEPGknRiEniec/giphy.gif',
+    'https://media.giphy.com/media/qLWdMYX1NYF2g/giphy.gif',
+    'https://media.giphy.com/media/Jq824R93JsLwZCaiSL/giphy.gif',
+    'https://media.giphy.com/media/suCJQGchI6oBW/giphy.gif',
+    'https://media.giphy.com/media/oOX5qIDkzDjeo/giphy.gif'
+  ],
+  lost: [
+    'https://media.giphy.com/media/a4pvOry0Nd4dy/giphy.gif',
+    'https://media.giphy.com/media/rKj0oXtnMQNwY/giphy.gif',
+    'https://media.giphy.com/media/H7kDjHfuqukEZoWhut/giphy.gif',
+    'https://media.giphy.com/media/1jARfPtdz7eE0/giphy.gif',
+    'https://media.giphy.com/media/suCJQGchI6oBW/giphy.gif',
+    'https://media.giphy.com/media/fw8XWwT5YuSJGBwddf/giphy.gif',
+    'https://media.giphy.com/media/10tIjpzIu8fe0/giphy.gif',
+    'https://media.giphy.com/media/O3JyUHiKqsviE/giphy.gif',
+    'https://media.giphy.com/media/12rTjvF0ANQJsk/giphy.gif',
+    'https://media.giphy.com/media/7VHV66bRjGRSo/giphy.gif',
+    'https://media.giphy.com/media/zFa3vjySsETPa/giphy.gif'
+  ]
 }
 
 function getRandomInt(max) {
