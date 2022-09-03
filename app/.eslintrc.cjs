e182550ebca0b22d6eeb89e843ee21a6846887d7
Gaute Meek Olsen <gaute.meek@gmail.com>
2022-09-03 14:50:02 +0200

    empty vue project

---
+++ app/.eslintrc.cjs
@@ -0,0 +1,12 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-typescript/recommended', 'prettier'],
+  env: {
+    'vue/setup-compiler-macros': true,
+  },
+  rules: {
+    'vue/return-in-computed-property': 'off',
+  },
+  ignorePatterns: ['postcss.config.js'],
+}
